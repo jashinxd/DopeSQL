8ef9bc289596b0fb447de83e16e075eea0f460e3
Jason Shin <jasonshin98@gmail.com>
2015-10-30 13:01:29 -0400

    changed addComments

--- Append.py
+++ Append.py
@@ -15,29 +15,28 @@ def GreatestStoryID():
 '''
 def register(username,password):
     connection = MongoClient()
-    db = connection['users']
+    db = connection['StoryBase']
     db.users.insert({'uname': username, 'pword': password})
 
 def validuname(username):
     connection = MongoClient()
-    db = connection['users']
+    db = connection['Story']
     curs = db.users.find({'uname':username})
     if curs.count() != 0:
         return True
     return False
     
 
-'''
+
 def comment(storyID, CContent, Date):
-    conn = sqlite3.connect("StoryBase.db")
-    c = conn.cursor()
-    q = """insert into comments values ('%s','%s','%s');""" % (storyID, CContent, Date)
-    c.execute(q)
-    conn.commit()
+    connection = MongoClient()
+    db = connection['Comments']
+    db.users.insert({'storyID': storyID, 'CContent': CContent, 'Date': Date})
+    
 def addStory(Content, Name, Username, ID, Date):
     conn = sqlite3.connect("StoryBase.db")
     c = conn.cursor()
     q = """insert into Stories values ('%s','%s','%s','%s','%s');""" % (Content,Name,Username,ID,Date)
     c.execute(q)
     conn.commit()
-'''
+
